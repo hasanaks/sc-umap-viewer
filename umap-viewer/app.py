372e675409acf57ba581fedb30e18449d94c7b16
hasanaks <hasanardaaks@gmail.com>
2026-02-11 18:46:16 +0300

    add functionality to find and open h5ad files from the uploaded zip

--- umap-viewer/app.py
+++ umap-viewer/app.py
@@ -1,7 +1,21 @@
 from shiny.express import ui, render, input
+from zipfile import ZipFile
 
 ui.input_file("dataset", "Select a dataset file (.zip)", accept=[".zip"])
 
+# @render.plot()
+# def dataset_plot():
+#     pass
+
 @render.text
-def txt():
-    return input.dataset()
+def file_contents():
+    dataset = input.dataset()
+
+    if dataset:
+        print("opened:", dataset)
+
+        with ZipFile(dataset[0]["datapath"]) as dataset_zip:
+            for fpath in dataset_zip.namelist():
+                if fpath.endswith(".h5ad"):
+                    with dataset_zip.open(fpath) as dataset_file:
+                        pass # todo: generate umap plot
