7e454383b86ecbeb68706b720a8cc46f27e6081c
hasanaks <hasanardaaks@gmail.com>
2026-02-12 21:18:01 +0300

    cleanup imports & rename variables

--- umap-viewer/app.py
+++ umap-viewer/app.py
@@ -1,17 +1,16 @@
 from shiny.express import ui, render, input
-import zipfile
 import scanpy as sc
 from h5ad import H5AD
 
-ui.input_file("dataset", "Select a dataset file (.zip)", accept=[".zip"])
+ui.input_file("dataset_file", "Select a dataset file (.zip)", accept=[".zip"])
 
 
 @render.plot(alt="UMAP Plot")
 def dataset_plot():
-    dataset = input.dataset()
+    dataset_file = input.dataset_file()
 
-    if dataset:
-        dataset = H5AD(dataset[0]["datapath"])
+    if dataset_file:
+        dataset = H5AD(dataset_file[0]["datapath"])
 
         axes = sc.pl.umap(dataset.adata, show=False, title=dataset.name)
         return axes
