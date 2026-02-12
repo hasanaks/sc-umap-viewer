e97984a1f37c10efebc4dc514761682561c528ad
hasanaks <hasanardaaks@gmail.com>
2026-02-12 22:16:46 +0300

    update the UI

--- umap_viewer/app.py
+++ umap_viewer/app.py
@@ -2,15 +2,19 @@ from shiny.express import ui, render, input
 import scanpy as sc
 from h5ad import H5AD
 
-ui.input_file("dataset_file", "Select a dataset file (.zip)", accept=[".zip"])
+ui.page_opts(title="UMAP Viewer", fillable=True)
+
+with ui.sidebar():
+    ui.input_file("dataset_file", "Select a dataset file (.zip)", accept=[".zip"])
 
 
-@render.plot(alt="UMAP Plot")
-def dataset_plot():
-    dataset_file = input.dataset_file()
+with ui.card(full_screen=True):
+    @render.plot(alt="UMAP Plot")
+    def dataset_plot():
+        dataset_file = input.dataset_file()
 
-    if dataset_file:
-        dataset = H5AD(dataset_file[0]["datapath"])
+        if dataset_file:
+            dataset = H5AD(dataset_file[0]["datapath"])
 
-        axes = sc.pl.umap(dataset.adata, show=False, title=dataset.name)
-        return axes
+            axes = sc.pl.umap(dataset.adata, show=False, title=dataset.name)
+            return axes
