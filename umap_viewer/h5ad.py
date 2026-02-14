9c9453fb0077eeccb6bf1476aa880664532fe168
hasanaks <hasanardaaks@gmail.com>
2026-02-14 19:01:14 +0300

    read h5ad files by extracting the zip

--- umap_viewer/h5ad.py
+++ umap_viewer/h5ad.py
@@ -1,25 +1,27 @@
-from typing import IO
+from tempfile import TemporaryDirectory
 import scanpy as sc
 from pathlib import Path
 import zipfile
 
 
 class H5AD:
-    # reads an .h5ad file from a file-like object
-    def __init__(self, file: IO[bytes]):
-        self.name = str(Path(file.name).with_suffix(""))
-        self.adata = sc.read_h5ad(file)
+    # reads an .h5ad file into AnnData
+    def __init__(self, fpath: str):
+        with open(fpath, "rb") as f:
+            self.name = Path(f.name).stem
+            self.adata = sc.read_h5ad(f)
 
 
 # reads and returns a list[H5AD] from a zip archive
 def h5ads_from_zip(zip_path: str) -> list[H5AD]:
     h5ads = list()
 
-    with zipfile.ZipFile(zip_path, "r") as z:
-        for file in z.namelist():
-            if file.endswith(".h5ad"):
-                with z.open(file) as dataset:
-                    h5ads.append(H5AD(dataset))
+    with TemporaryDirectory() as temp:
+        with zipfile.ZipFile(zip_path, "r") as z:
+            z.extractall(temp)
+
+        for h5ad_file in Path(temp).glob("*.h5ad"):
+            h5ads.append(H5AD(h5ad_file))
 
     if len(h5ads) == 0:
         raise FileNotFoundError(
